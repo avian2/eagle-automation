e3f8885bb8954906f8d6f463a110c70626f5de2b
Bernard Pratz <guyzmo+github@m0g.net>
2015-08-18 16:23:05 +0100

    improved setup.py
    Signed-off-by: Bernard Pratz <guyzmo+github@m0g.net>

--- setup.py
+++ setup.py
@@ -1,21 +1,51 @@
 #!/usr/bin/python
+# -+- encoding: utf-8
 
 from setuptools import setup
 
 import sys
+
+opts = dict()
 if sys.platform == 'win32':
 	import py2exe
+	opts.update(
+		dict(
+			zipfile=None,
+			options={
+				'py2exe':{
+					'includes': ['docopt'],
+					'bundle_files': 1
+				}
+			},
+			console=["eagle_automation/pea.py"],
+		))
+	# windows=[
+	#     {
+	#         'script': 'eagle_automation/artool.py',
+	#         # 'icon_resources': [(1, 'moduleicon.ico')]
+	#     }
+	# ],
+
+# We do not need pandoc when installing through pypi
+if sys.argv[0] == 'setup.py':
+    opts.update(dict(
+        long_description_markdown_filename='README.md',
+        setup_requires=['setuptools_markdown'],
+    ))
+else:
+    opts.update(dict(
+        long_description=open('README.md', 'r'),
+    ))
 
 setup(
 	name='eagle_automation',
-	version='0.1.0',
+	version='0.1.4',
 	description='Simple scripts supporting open hardware development using CadSoft EAGLE',
 	license='GPL',
 	author='Tomaz Solc, Bernard Pratz',
 	author_email='tomaz.solc@tablix.org, guyzmo+pea@m0g.net',
-	url='',
+    url='https://github.com/guyzmo/eagle-automation',
 	packages=['eagle_automation'],
-	long_description_markdown_filename='README.md',
 	classifiers=[
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.4',
@@ -25,33 +55,19 @@ setup(
 		'License :: OSI Approved',
 		'Operating System :: Unix',
 	],
-	data_files = [('lib/python/eagle_automation', ['eagle_automation/default.conf'])],
-	setup_requires=['setuptools-markdown'],
 	install_requires=[
 		'pillow',
 		'docopt',
 		'setuptools',
 	],
-	# windows=[
-	#     {
-	#         'script': 'eagle_automation/artool.py',
-	#         # 'icon_resources': [(1, 'moduleicon.ico')]
-	#     }
-	# ],
-	zipfile=None,
-	options={'py2exe':{
-		'includes': ['docopt'],
-		'bundle_files': 1
-	}
-	},
 	entry_points="""
 	# -*- Entry points: -*-
 	[console_scripts]
 	pea = eagle_automation.pea:main
 	""",
-	console=["eagle_automation/pea.py"],
+    **opts
 )
 
-print("""\
-To start using this tool, after `python setup.py install` do `pea --help`
-""")
+# offer a nice message when doing pip install
+if sys.argv[0] == 'setup.py' and 'install' in sys.argv:
+    print("🍻  To start using this tool, do `pea --help`")
